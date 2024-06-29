007672d3285715a29d2b3ad3b7aaa03a66c99967
ZafCode14 <zafarwp14@gmail.com>
2024-06-29 18:51:02 +0300

    mand

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
-const bodyParser = require('body-parser');
-import routes from './routes/index.js';
+import controllerRouting from './routes/index';
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use('/', routes);
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+controllerRouting(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
